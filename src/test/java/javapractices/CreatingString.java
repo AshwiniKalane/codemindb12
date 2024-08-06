659b403d82c4991a5071c5c36e9da3caf28cfe9e
Aayu <ashukalane@gmail.com>
2024-08-06 17:19:48 +0530

    added creatingstring class

---
+++ src/test/java/javapractices/CreatingString.java
@@ -0,0 +1,23 @@
+package javapractices;
+
+public class CreatingString {
+
+	public static void main(String[] args) {
+
+		// Create String by using String literal
+		String s = "Welcome to codemind";
+		String s1 = "123312DFGerfr$%%$df";
+
+		System.out.println(s);
+
+		// Create String by using new keyword
+		String s2 = new String("Welcome to pune");
+		char a[] = { 'j', 'a', 'v', 'a' };
+		String s3 = new String(a);
+
+		System.out.println(s3);
+
+	}
+
+
+}

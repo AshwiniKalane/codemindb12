139ffe1ffb929c3e1e1332064dd0f87c6a5fb8d7
Aayu <ashukalane@gmail.com>
2024-08-06 23:58:28 +0530

    adding string program for immutable ,not immutable and sample program

---
+++ src/test/java/assignment/TestStringImmutable.java
@@ -0,0 +1,19 @@
+package assignment;
+
+public class TestStringImmutable {
+
+	public static void main(String[] args) {
+		String empName = "Ashwini";
+		String firstName = "Ashwini";
+		String employee = "Ashwini";
+
+		empName.concat("kalane");
+		System.out.println(empName);
+
+		System.out.println(firstName);
+
+		System.out.println(employee);
+
+	}
+
+}
---
+++ src/test/java/assignment/TestStringIsNotImmutable.java
@@ -0,0 +1,32 @@
+package assignment;
+
+public class TestStringIsNotImmutable {
+
+	public static void main(String[] args) {
+		
+
+		
+
+			
+				String empName = "Ashwini";
+				String firstName = "Ashwini";
+				String employee = "Ashwini";
+
+				String ee=empName.concat("kalane");
+				
+				System.out.println(ee);
+				
+				System.out.println(empName);
+
+				System.out.println(firstName);
+
+				System.out.println(employee);
+
+			}
+
+		
+
+
+	}
+
+
